cb897e0893926097d4bf1eb6d0fee931d66523cb
Nicholas Wiersma <nick@wiersma.co.za>
2016-01-03 09:40:34 +0200

    Added mongodb pecl.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -55,7 +55,8 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 apt-get install -y --force-yes php7.0-cli php7.0-dev \
 php-pgsql php-sqlite3 php-gd \
 php-curl php7.0-dev \
-php-imap php-mysql php-memcached php7.0-readline
+php-imap php-mysql php-memcached php7.0-readline \
+php-mongodb
 
 # Install Composer
 
