7e4d607562f5a8944f72a1141841a77e8e566945
Nicholas Wiersma <nick@wiersma.co.za>
2016-03-06 10:07:12 +0200

    Minor build fix.

--- build-parallels.sh
+++ build-parallels.sh
@@ -11,7 +11,7 @@ time vagrant up --provider parallels 2>&1 | tee parallels-build-output.log
 vagrant halt
 # shrink disk (assumes running on osx)
 prl_disk_tool compact --hdd ~/Documents/Parallels/settler_*/harddisk1.hdd
-## 'vagrant package' does not work with parallels boxes (http://parallels.github.io/vagrant-parallels/docs/boxes/base.html)
+# 'vagrant package' does not work with parallels boxes (http://parallels.github.io/vagrant-parallels/docs/boxes/base.html)
 rm -f ~/Documents/Parallels/settler_*/*.log
 cp -r ~/Documents/Parallels/settler_* box.pvm
 tar cvzf parallels.box ./box.pvm ./metadata.json
